f3b3d05b539a7ff43326116deb7410bfaa9664fb
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2023-05-07 21:15:41 +0500

    build: footer and contact

--- js/router.js
+++ js/router.js
@@ -8,9 +8,7 @@ const route = (event) => {
 const routes = {
     404: "/pages/404.html",
     "/": "/pages/index.html",
-    "/about": "/pages/about.html",
-    "/lorem": "/pages/lorem.html",
-    "/test": "/pages/test.html",
+    "/contact": "/pages/contact.html",
 };
 
 const handleLocation = async () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,17 +17,22 @@ module.exports = {
         },
       },
       animation: {
-        blob: "blob 7s infinite",
-        animation: "gradient 5s ease-in-out infinite alternate",
+        handAnimation: "handAnimation 2s linear infinite",
       },
       keyframes: {
-        gradient: {
-          "0%": {
-            "background-position": "0% 50%",
+        handAnimation: {
+          "0%": { transform: "rotate( 0.0deg)" },
+          "10%": {
+            transform: "rotate(14.0deg)",
           },
-          "100%": {
-            "background-position": "100% 50%",
+          "20%": { transform: "rotate(-8.0deg)" },
+          "30%": { transform: "rotate(14.0deg) " },
+          "40%": { transform: "rotate(-4.0deg) " },
+          "50%": { transform: "rotate(10.0deg) " },
+          "60%": {
+            transform: "rotate(0.0deg)",
           },
+          "100%": { transform: "rotate(0.0deg)" },
         },
       },
     },
