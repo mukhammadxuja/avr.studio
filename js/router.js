8b38e93ffe6648b25305331e8c29eeacd77aa1e5
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2023-05-08 23:14:36 +0500

    build: dotsoft page and blog section

--- js/router.js
+++ js/router.js
@@ -1,21 +1,22 @@
 const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
 };
 
 const routes = {
-    404: "/pages/404.html",
-    "/": "/pages/index.html",
-    "/contact": "/pages/contact.html",
+  404: "/pages/404.html",
+  "/": "/pages/index.html",
+  "/contact": "/pages/contact.html",
+  "/project/dotsoft": "/pages/dotsoftuz.html",
 };
 
 const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
 };
 
 window.onpopstate = handleLocation;
