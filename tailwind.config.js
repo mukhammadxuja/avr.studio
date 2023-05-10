7fdfc43b7a9c731ce58c2b3d4edde857a3132c58
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2023-05-10 19:26:47 +0500

    bugfix: added meta tags for fix media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      screens: {
-        sm: { max: "639px" },
-        md: { max: "767px" },
-        lg: { max: "1023px" },
-        xl: { max: "1279px" },
-        "2xl": { max: "1535px" },
-      },
       container: {
         padding: {
           DEFAULT: "2rem",
