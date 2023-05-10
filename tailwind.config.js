84f7d37fbd782b080240e67f248ab9c71b490e2c
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2023-05-10 22:29:46 +0500

    style: mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     extend: {
       container: {
         padding: {
-          DEFAULT: "2rem",
+          DEFAULT: "1rem",
           sm: "2rem",
           lg: "3rem",
           xl: "4rem",
